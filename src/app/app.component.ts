daf22dba5549507d797a8496b88c62cc8a9bd387
XyoloJR <xyolo@msn.com>
2018-05-29 08:23:44 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './models/post';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  posts : Post[];
+
+  constructor(){
+    this.posts = new Array();
+    this.posts.push({title: 'prem\'s', content: 'mon premier post', loveIts: 0, created_at: new Date()})
+  }
 }
---
+++ src/app/models/post.ts
@@ -0,0 +1,8 @@
+export interface Post {
+  title: string;
+  content: string;
+  loveIts: number;
+  created_at: Date;
+
+  love();
+}
