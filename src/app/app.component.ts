a895dbbd1de8ff0e136fcb4bb016da190cece7ef
XyoloJR <xyolo@msn.com>
2018-06-03 17:53:00 +0200

    post-service provide Subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,7 @@ import { Post } from './models/post';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public posts: Post[];
 
-  constructor() {
-    this.posts = new Array();
-    this.posts.push(
-      // tslint:disable-next-line:max-line-length
-      new Post('prem\'s', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam rutrum nisl ut sapien placerat aliquam a nec dui. Donec vehicula nisi diam, eu pharetra nibh mattis at. Sed pharetra ornare molestie. Quisque vel lectus aliquet ligula semper ultrices non vel nunc. Integer vulputate pretium tempus. Quisque at mollis risus. Vivamus vestibulum dapibus pharetra. Vestibulum nec ligula pretium mauris semper fringilla vitae a justo. Praesent sollicitudin eros augue, sed consectetur erat pulvinar vel. Aliquam sed mi dui. Praesent aliquam lorem eget urna vulputate blandit.')
-    );
-    this.posts.push(
-      new Post('deuze', 'Et là c\'est le drame')
-    );
-    this.posts.push(
-      new Post('troize', 'Encore un peu de texte par ici')
-    );
-    this.posts.push(
-      new Post('quartz', 'Un dernier petit message inintéressant au possible')
-    );
-    this.posts[0].love();
-    this.posts[2].dontLove();
-  }
+  constructor() {}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PostListComponent } from './components/post-list/post-list.component';
 import { PostListItemComponent } from './components/post-list-item/post-list-item.component';
+import { NewPostComponent } from './components/new-post/new-post.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponent,
-    PostListItemComponent
+    PostListItemComponent,
+    NewPostComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
